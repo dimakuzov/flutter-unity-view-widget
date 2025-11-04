1ab57688f5169f2882d2a2eb58bf4236382e4ff1
dima <dima.kuzov@gmail.com>
2025-11-04 23:12:02 +0100

    disable mapbox plugin

--- example/unity/SocialBeeAR/Assets/Scripts/MapBoxManager.cs
+++ example/unity/SocialBeeAR/Assets/Scripts/MapBoxManager.cs
@@ -2,11 +2,11 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using ARLocation;
-using Mapbox.CheapRulerCs;
-using Mapbox.Examples;
-using Mapbox.Unity.Map;
-using Mapbox.Unity.Utilities;
-using Mapbox.Utils;
+// using Mapbox.CheapRulerCs;
+// using Mapbox.Examples;
+// using Mapbox.Unity.Map;
+// using Mapbox.Unity.Utilities;
+// using Mapbox.Utils;
 using Niantic.Lightship.AR.VpsCoverage;
 using SocialBeeAR;
 using UnityEngine;
@@ -16,7 +16,7 @@ using UnityEngine.UI;
 public class MapBoxManager : BaseSingletonClass<MapBoxManager>
 {
     [SerializeField]
-    public AbstractMap _map;
+    // public AbstractMap _map;
     public GameObject mapEssentials;
     public Camera mapCamera;
     public GameObject navigationMapItem;
@@ -27,13 +27,13 @@ public class MapBoxManager : BaseSingletonClass<MapBoxManager>
     public GameObject playerMarkerPrefab;
     public float markerSpawnScale = 10;
     private GameObject currentSelectedMarker = null;
-    [SerializeField]
-    [Geocode]
-    List<string> _locationStrings;
-    Vector2d[] _locations;
-    [Geocode]
-    public string _playerLocationString;
-    Vector2d _locationPlayer;
+    // [SerializeField]
+    // [Geocode]
+    // List<string> _locationStrings;
+    // Vector2d[] _locations;
+    // [Geocode]
+    // public string _playerLocationString;
+    // Vector2d _locationPlayer;
     public List<GameObject> _spawnedObjects;
     GameObject spawnedPlayer;
 
@@ -59,43 +59,43 @@ public class MapBoxManager : BaseSingletonClass<MapBoxManager>
     public void ApplyCameraSetting(float cameraFov, float cameraRectWidth,float cameraRectHeight,bool interactionStatus)
     {
 
-        mapCamera.fieldOfView = cameraFov;
-        mapCamera.rect = new Rect(0,0, cameraRectWidth, cameraRectHeight);
-        _map.gameObject.GetComponent<QuadTreeCameraMovement>().enabled = interactionStatus;
-        MarkerInteraction(interactionStatus);
+        // mapCamera.fieldOfView = cameraFov;
+        // mapCamera.rect = new Rect(0,0, cameraRectWidth, cameraRectHeight);
+        // _map.gameObject.GetComponent<QuadTreeCameraMovement>().enabled = interactionStatus;
+        // MarkerInteraction(interactionStatus);
     }
 
     public void SetMapView(MapView currentMapView, bool willNavigate = false)
     {
-        PointsBarManager.Instance.HidePointsBar();
-        if (currentMapView == MapView.FullViewMap)
-        {
-            spawnedPlayer.GetComponent<RotateWithLocationProvider>().enabled = false;
-            spawnedPlayer.transform.GetChild(0)?.gameObject.SetActive(true);
-            spawnedPlayer.transform.GetChild(1)?.gameObject.SetActive(false);
-            navigationMapItem.SetActive(false);
-            navigationMapCamera.SetActive(false);
-            mapCamera.gameObject.SetActive(true);
-        }
-        else
-        {
-            spawnedPlayer.transform.GetChild(0)?.gameObject.SetActive(false);
-            spawnedPlayer.transform.GetChild(1)?.gameObject.SetActive(true);
-            navigationMapCamera.transform.position = mapCamera.transform.position;
-            navigationMapCamera.gameObject.SetActive(true);
-            navigationMapItem.SetActive(true);
-            spawnedPlayer.GetComponent<RotateWithLocationProvider>().enabled = true;
-            mapCamera.gameObject.SetActive(false);
-        }
-
-        isNavigatingToActivity = willNavigate;
-        print($"isNavigatingToActivity={isNavigatingToActivity}");
-        if (isNavigatingToActivity && SystemInfo.deviceUniqueIdentifier == SBContextManager.VonsDeviceID)
-        {
-            debugProbingStartTime = DateTime.Now;
-        }
-        // hasUpdatedUIForPreLocalization = false;
-        hasUpdatedUIForPreLocalizationAfterNavigation = false;
+        // PointsBarManager.Instance.HidePointsBar();
+        // if (currentMapView == MapView.FullViewMap)
+        // {
+        //     spawnedPlayer.GetComponent<RotateWithLocationProvider>().enabled = false;
+        //     spawnedPlayer.transform.GetChild(0)?.gameObject.SetActive(true);
+        //     spawnedPlayer.transform.GetChild(1)?.gameObject.SetActive(false);
+        //     navigationMapItem.SetActive(false);
+        //     navigationMapCamera.SetActive(false);
+        //     mapCamera.gameObject.SetActive(true);
+        // }
+        // else
+        // {
+        //     spawnedPlayer.transform.GetChild(0)?.gameObject.SetActive(false);
+        //     spawnedPlayer.transform.GetChild(1)?.gameObject.SetActive(true);
+        //     navigationMapCamera.transform.position = mapCamera.transform.position;
+        //     navigationMapCamera.gameObject.SetActive(true);
+        //     navigationMapItem.SetActive(true);
+        //     spawnedPlayer.GetComponent<RotateWithLocationProvider>().enabled = true;
+        //     mapCamera.gameObject.SetActive(false);
+        // }
+        //
+        // isNavigatingToActivity = willNavigate;
+        // print($"isNavigatingToActivity={isNavigatingToActivity}");
+        // if (isNavigatingToActivity && SystemInfo.deviceUniqueIdentifier == SBContextManager.VonsDeviceID)
+        // {
+        //     debugProbingStartTime = DateTime.Now;
+        // }
+        // // hasUpdatedUIForPreLocalization = false;
+        // hasUpdatedUIForPreLocalizationAfterNavigation = false;
     }
 
     /// <summary>
@@ -104,45 +104,45 @@ public class MapBoxManager : BaseSingletonClass<MapBoxManager>
     /// <param name="Wayspots"></param>
     public void GenerateMap(List<LocalizationTarget> Wayspots)
     {
-
-        // Destroy previous markers if any...
-        DestroyAllMarkers();
-
-
-
-        _map.Options.locationOptions.latitudeLongitude = VpsCoverageManager.Instance.LastKnownUserLocation != null
-            ? $"{VpsCoverageManager.Instance.LastKnownUserLocation!.Value.Latitude},{VpsCoverageManager.Instance.LastKnownUserLocation!.Value.Longitude}"
-            : _playerLocationString;
-
-        Debug.Log("Current Location of Map is " + _map.Options.locationOptions.latitudeLongitude);
-        _map.gameObject.GetComponent<QuadTreeCameraMovement>().enabled = false;
-        ApplyCameraSetting(60, 1, 1,true);
-        mapEssentials.SetActive(true);
-
-        _spawnedObjects = new List<GameObject>();
-        _locationStrings = new List<string>();
-
-        
-        for (int i = 0; i < Wayspots.Count; i++)
-        {
-            var spawnedWayspot = Instantiate(wayspotMarkerPrefab);
-            spawnedWayspot.transform.localScale = new Vector3(markerSpawnScale, markerSpawnScale, markerSpawnScale);
-            spawnedWayspot.gameObject.GetComponent<WayspotMarker>().wayspotData = Wayspots[i];
-            spawnedWayspot.name = spawnedWayspot.gameObject.GetComponent<WayspotMarker>().wayspotData.Identifier;
-            StartCoroutine(LoadImageFromURL(spawnedWayspot.gameObject.GetComponent<WayspotMarker>().wayspotData.ImageURL,
-               spawnedWayspot.gameObject.GetComponent<WayspotMarker>().markerThumbnail));
-            var currentLatLong = spawnedWayspot.gameObject.GetComponent<WayspotMarker>().wayspotData.Center.Latitude.ToString() + ", " +
-                spawnedWayspot.gameObject.GetComponent<WayspotMarker>().wayspotData.Center.Longitude
-                .ToString();
-            _locationStrings.Add(currentLatLong);
-            _spawnedObjects.Add(spawnedWayspot);
-
-        }
-
-        _locations = new Vector2d[_locationStrings.Count];
-        PlaceMarkers();
-        SetMapView(MapView.FullViewMap);
-        ActivityUIFacade.Instance.FireUIEvent(ActivityUIFacade.UIEvent.ShowVpsRequestPanel);
+        //
+        // // Destroy previous markers if any...
+        // DestroyAllMarkers();
+        //
+        //
+        //
+        // _map.Options.locationOptions.latitudeLongitude = VpsCoverageManager.Instance.LastKnownUserLocation != null
+        //     ? $"{VpsCoverageManager.Instance.LastKnownUserLocation!.Value.Latitude},{VpsCoverageManager.Instance.LastKnownUserLocation!.Value.Longitude}"
+        //     : _playerLocationString;
+        //
+        // Debug.Log("Current Location of Map is " + _map.Options.locationOptions.latitudeLongitude);
+        // _map.gameObject.GetComponent<QuadTreeCameraMovement>().enabled = false;
+        // ApplyCameraSetting(60, 1, 1,true);
+        // mapEssentials.SetActive(true);
+        //
+        // _spawnedObjects = new List<GameObject>();
+        // _locationStrings = new List<string>();
+        //
+        //
+        // for (int i = 0; i < Wayspots.Count; i++)
+        // {
+        //     var spawnedWayspot = Instantiate(wayspotMarkerPrefab);
+        //     spawnedWayspot.transform.localScale = new Vector3(markerSpawnScale, markerSpawnScale, markerSpawnScale);
+        //     spawnedWayspot.gameObject.GetComponent<WayspotMarker>().wayspotData = Wayspots[i];
+        //     spawnedWayspot.name = spawnedWayspot.gameObject.GetComponent<WayspotMarker>().wayspotData.Identifier;
+        //     StartCoroutine(LoadImageFromURL(spawnedWayspot.gameObject.GetComponent<WayspotMarker>().wayspotData.ImageURL,
+        //        spawnedWayspot.gameObject.GetComponent<WayspotMarker>().markerThumbnail));
+        //     var currentLatLong = spawnedWayspot.gameObject.GetComponent<WayspotMarker>().wayspotData.Center.Latitude.ToString() + ", " +
+        //         spawnedWayspot.gameObject.GetComponent<WayspotMarker>().wayspotData.Center.Longitude
+        //         .ToString();
+        //     _locationStrings.Add(currentLatLong);
+        //     _spawnedObjects.Add(spawnedWayspot);
+        //
+        // }
+        //
+        // _locations = new Vector2d[_locationStrings.Count];
+        // PlaceMarkers();
+        // SetMapView(MapView.FullViewMap);
+        // ActivityUIFacade.Instance.FireUIEvent(ActivityUIFacade.UIEvent.ShowVpsRequestPanel);
     }
 
     /// <summary>
@@ -150,39 +150,39 @@ public class MapBoxManager : BaseSingletonClass<MapBoxManager>
     /// </summary>
     void PlaceMarkers()
     {
-        for (int i = 0; i < _locationStrings.Count; i++)
-        {
-            var locationString = _locationStrings[i];
-            _locations[i] = Conversions.StringToLatLon(locationString);
-            _spawnedObjects[i].transform.localPosition = _map.GeoToWorldPosition(_locations[i], true);
-            Debug.Log("Current Position is " + _spawnedObjects[i].transform.localPosition);
-
-        }
-
-        // Place Player Marker...
-        var playerLocation = VpsCoverageManager.Instance.LastKnownUserLocation != null
-            ? $"{VpsCoverageManager.Instance.LastKnownUserLocation!.Value.Latitude},{VpsCoverageManager.Instance.LastKnownUserLocation!.Value.Longitude}"
-            : _playerLocationString;
-        Debug.Log($"User Location: {playerLocation}");
-        PlaceUserMarkerOnMap(playerLocation);
-
-        loadingPanel.SetActive(false);
-        _map.gameObject.GetComponent<QuadTreeCameraMovement>().enabled = true;
-        isMapLoading = false;
+        // for (int i = 0; i < _locationStrings.Count; i++)
+        // {
+        //     var locationString = _locationStrings[i];
+        //     _locations[i] = Conversions.StringToLatLon(locationString);
+        //     _spawnedObjects[i].transform.localPosition = _map.GeoToWorldPosition(_locations[i], true);
+        //     Debug.Log("Current Position is " + _spawnedObjects[i].transform.localPosition);
+        //
+        // }
+        //
+        // // Place Player Marker...
+        // var playerLocation = VpsCoverageManager.Instance.LastKnownUserLocation != null
+        //     ? $"{VpsCoverageManager.Instance.LastKnownUserLocation!.Value.Latitude},{VpsCoverageManager.Instance.LastKnownUserLocation!.Value.Longitude}"
+        //     : _playerLocationString;
+        // Debug.Log($"User Location: {playerLocation}");
+        // PlaceUserMarkerOnMap(playerLocation);
+        //
+        // loadingPanel.SetActive(false);
+        // _map.gameObject.GetComponent<QuadTreeCameraMovement>().enabled = true;
+        // isMapLoading = false;
     }
 
     public void PlaceUserMarkerOnMap(string playerLocation)
     {
-        // Place Player Marker...
-        var player = Instantiate(playerMarkerPrefab);
-        // var playerLocation = VpsCoverageManager.Instance.LastKnownUserLocation != null
-        //     ? $"{VpsCoverageManager.Instance.LastKnownUserLocation!.Value.Latitude},{VpsCoverageManager.Instance.LastKnownUserLocation!.Value.Longitude}"
-        //     : _playerLocationString;
-        Debug.Log($"User Location: {playerLocation}");
-        _locationPlayer = Conversions.StringToLatLon(playerLocation);
-        player.transform.localPosition = _map.GeoToWorldPosition(_locationPlayer, true);
-        player.transform.localScale = new Vector3(markerSpawnScale, markerSpawnScale, markerSpawnScale);
-        spawnedPlayer = player;
+        // // Place Player Marker...
+        // var player = Instantiate(playerMarkerPrefab);
+        // // var playerLocation = VpsCoverageManager.Instance.LastKnownUserLocation != null
+        // //     ? $"{VpsCoverageManager.Instance.LastKnownUserLocation!.Value.Latitude},{VpsCoverageManager.Instance.LastKnownUserLocation!.Value.Longitude}"
+        // //     : _playerLocationString;
+        // Debug.Log($"User Location: {playerLocation}");
+        // _locationPlayer = Conversions.StringToLatLon(playerLocation);
+        // player.transform.localPosition = _map.GeoToWorldPosition(_locationPlayer, true);
+        // player.transform.localScale = new Vector3(markerSpawnScale, markerSpawnScale, markerSpawnScale);
+        // spawnedPlayer = player;
     }
 
     private void Update()
@@ -193,10 +193,10 @@ public class MapBoxManager : BaseSingletonClass<MapBoxManager>
         {
             for (int i = 0; i < count; i++)
             {
-                var spawnedObject = _spawnedObjects[i];
-                var location = _locations[i];
-                spawnedObject.transform.localPosition = _map.GeoToWorldPosition(location, true);
-                spawnedObject.transform.localScale = new Vector3(markerSpawnScale, markerSpawnScale, markerSpawnScale);
+                // var spawnedObject = _spawnedObjects[i];
+                // var location = _locations[i];
+                // spawnedObject.transform.localPosition = _map.GeoToWorldPosition(location, true);
+                // spawnedObject.transform.localScale = new Vector3(markerSpawnScale, markerSpawnScale, markerSpawnScale);
             }
         }
        
@@ -204,8 +204,8 @@ public class MapBoxManager : BaseSingletonClass<MapBoxManager>
         // For Player Marker.
         if (spawnedPlayer != null)
         {
-            spawnedPlayer.transform.localPosition = _map.GeoToWorldPosition(_locationPlayer, true);
-            spawnedPlayer.transform.localScale = new Vector3(markerSpawnScale, markerSpawnScale, markerSpawnScale);
+            // spawnedPlayer.transform.localPosition = _map.GeoToWorldPosition(_locationPlayer, true);
+            // spawnedPlayer.transform.localScale = new Vector3(markerSpawnScale, markerSpawnScale, markerSpawnScale);
         }
         
         UpdateLocalization();
@@ -240,13 +240,14 @@ public class MapBoxManager : BaseSingletonClass<MapBoxManager>
     /// <returns></returns>
     public float DistanceToDisplay(LocalizationTarget? currentSelctedMarker)
     {
-        if (currentSelctedMarker == null) return int.MaxValue;
-        
-        double[] playerLocation = new double[] { VpsCoverageManager.Instance.LastKnownUserLocation!.Value.Latitude,
-            VpsCoverageManager.Instance.LastKnownUserLocation!.Value.Longitude };
-        double[] currentMarkerLocation = new double[] { currentSelctedMarker.Value.Center.Latitude, currentSelctedMarker.Value.Center.Longitude };
-        CheapRuler cr = new CheapRuler(playerLocation[1], CheapRulerUnits.Meters);
-        return (float)cr.Distance(playerLocation, currentMarkerLocation);
+        // if (currentSelctedMarker == null) return int.MaxValue;
+        //
+        // double[] playerLocation = new double[] { VpsCoverageManager.Instance.LastKnownUserLocation!.Value.Latitude,
+        //     VpsCoverageManager.Instance.LastKnownUserLocation!.Value.Longitude };
+        // double[] currentMarkerLocation = new double[] { currentSelctedMarker.Value.Center.Latitude, currentSelctedMarker.Value.Center.Longitude };
+        // CheapRuler cr = new CheapRuler(playerLocation[1], CheapRulerUnits.Meters);
+        // return (float)cr.Distance(playerLocation, currentMarkerLocation);
+        return 0;
     }
 
     /// <summary>
@@ -264,9 +265,9 @@ public class MapBoxManager : BaseSingletonClass<MapBoxManager>
     /// </summary>
     public void ClearSpawnedMarkers()
     {
-        _locationStrings.Clear();
-        foreach (var item in _spawnedObjects)
-            Destroy(item);
+        // _locationStrings.Clear();
+        // foreach (var item in _spawnedObjects)
+        //     Destroy(item);
     }
 
     void MarkerInteraction(bool interactable)
